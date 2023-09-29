6d1c33c3c09d51642fe82e8d963f5efa450aa007
kperfiliev <kperfiliev@plu.edu>
2023-09-28 19:01:56 -0700

    got three button on display

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello from Mary! quack </Text>
+      <Text>Welcome to the Tamagatchi Game </Text>
+      <Text>Please choose the mode you want to play</Text>
       <StatusBar style="auto" />
+      
+      <Button
+        title="Pet Mode"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      <Button
+        title="Combat Mode"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      <Button
+        title="Story Mode"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
+
     </View>
+
   );
 }
 
@@ -18,3 +36,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// the code to run the program is npx expo run
+//git checkout kperfiliev
