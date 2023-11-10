13ba0b8ee977be2171340640818e35fb746b3402
Mary Nguyen <mary.nguyenwa18@gmail.com>
2023-11-10 07:31:55 -0800

    added explosion effect after button click

--- components/CombatModeLogic.js
+++ components/CombatModeLogic.js
@@ -1,58 +1,54 @@
-import React from 'react';
-export class CombatModeLogic {
-    // constructor() {
-    //   this.currentTurn = false;
-    //   this.roundCount = 0;
-    //   this.finalResult = false;
-    //   this.potentialMoves = ["rock", "paper", "scissors"];
-    //   this.playerMove = "";
-    // }
+import React, { useState, Component } from 'react';
+import { Modal } from 'react';
 
-    playerMove = "";
-  
-    setMove(move) {
-        this.playerMove = move;
-    }
-
-    handlePaperPress = () => {
-      this.setMove("paper");
-      console.log("set to paper.");
+class CombatModeLogic extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playerMove: "",
     };
-  
-    handleRockClick = () => {
-      this.setMove("rock");
-      console.log("set to rock.");
-    };
-  
-    handleScissorClick = () => {
-      this.setMove("scissor");
-      console.log("set to scissors.");
-    };
-  
-    updateOppHealth() {
-      //Update the opponents health after they've received a hit from the player.
-    }
-  
-    updatePlayerHealth() {
-      //Update the players health after they've received a hit from the player.
-    }
-  
-    getW() {
-    
-      return true; 
-    }
-  
-    getL() {
-    
-      return false;
-    }
-  
-    getFinalWinner() {
-      // Implement the logic for determining the final winner here.
-      return this.finalResult; // Placeholder, replace with actual logic
-    }
-  
-    rewardsEarned() {
-      // Implement the logic for calculating rewards here.
-    }
   }
+
+  handlePaperClick = () => {
+    this.setMove("paper");
+  };
+
+  handleRockClick = () => {
+    this.setMove("rock");
+  };
+
+  handleScissorClick = () => {
+    this.setMove("scissor");
+  };
+
+  setMove = (move) => {
+    this.setState({ playerMove: move });
+  };
+
+  updateOppHealth() {
+    // Update the opponent's health after they've received a hit from the player.
+  }
+
+  updatePlayerHealth() {
+    // Update the player's health after they've received a hit from the opponent.
+  }
+
+  getW() {
+    return true;
+  }
+
+  getL() {
+    return false;
+  }
+
+  getFinalWinner() {
+    // Implement the logic for determining the final winner here.
+    return this.finalResult; // Placeholder, replace with actual logic
+  }
+
+  rewardsEarned() {
+    // Implement the logic for calculating rewards here.
+  }
+}
+
+export default CombatModeLogic;
--- pages/combat/BattleScreen.js
+++ pages/combat/BattleScreen.js
@@ -1,43 +1,64 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions, ImageBackground, Image, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Dimensions, ImageBackground, Image, TouchableOpacity, Modal } from 'react-native';
 import backgroundImage from '../../images/background.gif';
 import rock from '../../images/rock.png';
 import scissor from '../../images/scissor.png';
 import paper from '../../images/paper.png';
-import player from '../../images/combatDuck2.gif';
-import healthBar from '../../images/healthBar.png';
-import opponent from '../../images/combatDuck.gif';
 import HealthBar from '../../modules/HealthBar';
 import Animal from '../../modules/BananaDuck';
 import OpponentDuck from '../../modules/OpponentDuck';
-//import CombatModeLogic from "../../components/CombatModeLogic";
+//import Explosion from '../../modules/Explosion';
+import explosion from '../../images/explosion.gif';
+//import {handleRockClick, handlePaperClick, handleScissorClick, isExplosionVisible} from '../../components/CombatModeLogic'
+//import { CombatModeLogic } from '../../components/CombatModeLogic';
 
 const window = Dimensions.get('window');
-const BattleScreen = ({ navigation }) => {
-// const combatLogic = new CombatModeLogic();
+const BattleScreen = ({ navigation}) => {
 
-return (
-  <View style={styles.container}>
-  <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
-  <View style= {styles.playerOppContainer}>
-    <HealthBar></HealthBar>
-    <View style={styles.animalContainer}>
-    <Animal></Animal>
+//const combatLogic = new CombatModeLogic();
+const [modalVisible, setModalVisible] = useState(false);
+
+  const handlePress = () => {
+    setModalVisible(true);
+    setTimeout(() => {
+      setModalVisible(false);
+    }, 1000);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+        <View style={styles.playerOppContainer}>
+          <HealthBar></HealthBar>
+          <View style={styles.animalContainer}>
+            <Animal></Animal>
+          </View>
+          <View style={styles.oppContainer}>
+            <OpponentDuck></OpponentDuck>
+            {modalVisible && <Image source={explosion} style={styles.explosionImage}></Image>}
+          </View>
+          <HealthBar></HealthBar>
+        </View>
+        <View style={styles.imageContainer}>
+          <TouchableOpacity onPress={() => handlePress()}>
+            <Image source={rock} style={styles.image} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handlePress()}>
+            <Image source={paper} style={styles.image} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handlePress()}>
+            <Image source={scissor} style={styles.image} />
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     </View>
-    <View style={styles.oppContainer}>
-    <OpponentDuck></OpponentDuck>
-    </View>
-    <HealthBar></HealthBar>
-  </View>
-    <View style={styles.imageContainer}>
-      <Image source={rock} style={styles.image}/>
-      <Image source={paper} style={styles.image}/>
-      <Image source={scissor} style={styles.image}/>
-    </View>
-  </ImageBackground>
-</View>
   );
-};
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -74,6 +95,14 @@ const styles = StyleSheet.create({
     marginLeft: 180,
     transform: [{ scaleX: -1 }],
     marginTop: -80
+  },
+  explosionImage: {
+    position: 'absolute',
+    top: 0,
+    left: 35,
+    right: 0,
+    bottom: 0,
+    resizeMode: 'contain',
   }
 });
 
