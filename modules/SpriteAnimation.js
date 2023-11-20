a5c9aec38aea381a6bb7f927852dc3a164e9de7e
Anyah Arboine <aarboine@plu.edu>
2023-11-19 22:54:42 -0800

    Fixed SpriteAnimation error

--- modules/SpriteAnimation.js
+++ modules/SpriteAnimation.js
@@ -1,34 +1,21 @@
 import React, { useRef } from 'react';
-import { View, StyleSheet } from 'react-native';
-import SpriteSheet from 'react-native-sprite-sheet'; // Make sure to import the correct module
+import { View, Button, StyleSheet, Image } from 'react-native';
+//import SpriteSheet from 'react-native-spritesheet';
 
 const SpriteAnimation = () => {
   const spriteSheetRef = useRef(null);
 
   const startAnimation = () => {
-    // Trigger animation by calling play method on the spriteSheetRef
-    spriteSheetRef.current.play({
-      type: 'linear',
-      frames: [0, 1, 2, 3], // Replace with your frame indices
-      loop: true,
-    });
+    //spriteSheetRef.current.play();
+    console.log("Animation is playing")
   };
-
-  const source = require('../images/CharacterSheet.png'); // Adjust the path based on your project structure
-  console.log('Image Source:', source); // Log the image source
 
   return (
     <View style={styles.container}>
-      {/* Add other components as needed */}
-      <SpriteSheet
-        ref={spriteSheetRef}
-        source={source}
-        columns={8} // Replace with the number of columns in your sprite sheet
-        rows={7} // Replace with the number of rows in your sprite sheet
-        width={100} // Replace with the width of each sprite frame
-        height={100} // Replace with the height of each sprite frame
-      />
-      {/* Add other components as needed */}
+
+      
+      
+      <Button title="Start Animation" onPress={startAnimation} />
     </View>
   );
 };
