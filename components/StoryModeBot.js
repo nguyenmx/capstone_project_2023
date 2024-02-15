fd383fcf6b8843ee5a25c2716d5c5b59a1d9bb56
kperfiliev <kperfiliev@plu.edu>
2024-02-15 14:43:10 -0800

    working on Dalle

--- components/StoryModeBot.js
+++ components/StoryModeBot.js
@@ -5,18 +5,41 @@ const testImage = 'https://as1.ftcdn.net/v2/jpg/06/15/90/08/1000_F_615900868_gO6
 export const generateResponse = async (userInput) => {
   // Check if the user is requesting an image
   const isImageRequest = userInput.toLowerCase().includes("selfie");
+  const isDalleRequest = userInput.toLowerCase().includes("dalle");
   if (isImageRequest) {
     // const imageURL = await generateDALLEResponse(userInput);
     // return imageURL; // Return the generated image URL
     return testImage;
     
-  } else {
+  } else if (isDalleRequest){
+    const dalleResponse = await generateDALLEResponse(userInput);
+    return dalleResponse;
+  }
+  
+  else {
     const chatGPTResponse = await generateChatGPTResponse(userInput);
     return chatGPTResponse; // Return the GPT-3.5 response
   }
 };
 
 const generateDALLEResponse = async (userInput) => {
+  try{
+    const response = await axios.post(
+      'https://api.openai.com/v1/images/generations', {
+        userInput,
+        n: 1,
+        size: "512x512"
+      }
+    );
+    let url = res?.data?.data[0]?.url;
+    console.log('got url of the image: ', url);
+    messages.push({role: 'assistant', content: url});
+    return Promise.resolve({success: true, data: messages});
+
+  }catch (error) {
+    console.error('Error generating image:', error);
+    return Promise.resolve({success: false, msg: error.message});
+  }
 };
 
 const generateChatGPTResponse = async (userInput) => {
