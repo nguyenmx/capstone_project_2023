19b3a733f146fd732754cb51f04a43bb9d80fa94
Mary Nguyen <mary.nguyenwa18@gmail.com>
2024-02-15 19:19:23 -0800

    Chat GPT 3.5 bot now works again but Dalle is still a work in progress.

--- components/StoryModeBot.js
+++ components/StoryModeBot.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { REACT_APP_API_KEY } from "@env";
 
-const testImage = 'https://as1.ftcdn.net/v2/jpg/06/15/90/08/1000_F_615900868_gO6CEKZefpOkrPaUEb1ykBHYzm5lhge2.jpg';
+const testImage = 'https://tse1.mm.bing.net/th/id/OIG4.V1MBi6cXS8sBkpVNmHCH?pid=ImgGn';
 export const generateResponse = async (userInput) => {
   // Check if the user is requesting an image
   const isImageRequest = userInput.toLowerCase().includes("selfie");
@@ -11,24 +11,30 @@ export const generateResponse = async (userInput) => {
     // return imageURL; // Return the generated image URL
     return testImage;
     
-  } else if (isDalleRequest){
+  } 
+  else if (isDalleRequest) {
     console.log("got here1");
     const dalleResponse = await generateDALLEResponse(userInput);
     return dalleResponse;
-    
   }
   
   else {
+    // Return the GPT-3.5 response
     const chatGPTResponse = await generateChatGPTResponse(userInput);
-    return chatGPTResponse; // Return the GPT-3.5 response
+    return chatGPTResponse;
   }
 };
 
+// const generateDALLEResponse = async (userInput) => {
+  
+// };
+
 const generateDALLEResponse = async (userInput) => {
   console.log("got here2");
-  try{
+  try {
     const response = await axios.post(
-      'https://api.openai.com/v1/images/generations', {
+      'https://api.openai.com/v1/images/generations',
+      {
         userInput,
         n: 1,
         size: "512x512"
@@ -41,7 +47,8 @@ const generateDALLEResponse = async (userInput) => {
       }
     );
     console.log("got here3");
-    return res?.data?.data[0]?.url;
+    console.log("DALL-E response URL:", response?.data?.data[0]?.url);
+    return response?.data?.data[0]?.url;
   } catch (error) {
     console.error('Error generating image:', error);
     return 'Error generating image';
